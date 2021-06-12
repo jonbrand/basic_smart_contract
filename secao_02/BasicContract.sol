8f3f8254680c770391735a7d5e10a22f2144d93b
jonbrand <jonbranaraujo18@gmail.com>
2021-06-11 22:30:26 -0300

    feat: types

--- secao_02/BasicContract.sol
+++ secao_02/BasicContract.sol
@@ -11,23 +11,62 @@ contract HelloWorld {
     address public userAddress;
     // booleano;
     bool public answer;
+    // mapeamento
+    mapping (address => uint) public hasInteracted;
+
 
     // memory: guarda o valor da variavel temporariamente, enquanto a função estiver ativa;
     function setText (string memory myText) public {
         text = myText;
+        setInteracted();
     }
 
     function setNumber (uint myNumber) public {
         number = myNumber;
+        setInteracted();
     }
 
     // função que pega o endereço do usuario;
     function setUserAddress () public {
         userAddress = msg.sender;
+        setInteracted();
     }
 
     // função booleana;
     function setAnswer(bool trueOrFalse) public {
         answer = trueOrFalse;
+        setInteracted();
+    }
+
+    // função de quantas vezes o usuário interage com o contrato;
+    function setInteracted() private {
+        hasInteracted[msg.sender] += 1;
+    }
+
+    // operações entre dois valores | funções puras não interagem com a blockchain (são de graça);
+    function sum(uint numb1, uint numb2) public pure returns(uint) {
+        return numb1 + numb2;
+    }
+
+    function sub(uint numb1, uint numb2) public pure returns(uint) {
+        return numb1 - numb2;
+    }
+
+    function mult(uint numb1, uint numb2) public pure returns(uint) {
+        return numb1 * numb2;
+    }
+
+    // solidity só trabalha com números inteiros;
+    function div(uint numb1, uint numb2) public pure returns(uint) {
+        return numb1 / numb2;
+    }
+
+    function pow(uint numb1, uint numb2) public pure returns(uint) {
+        return numb1 ** numb2;
+    }
+
+    // funções de consulta dentro da blockchain assume "view";
+    function sumStored(uint num1) public view returns(uint) {
+        return num1 + number;
     }
 }
